a36091f07de5ffbb5141021c37b43e99e0d0c6f4
swapnesh <swapneshhessonite@gmail.com>
2025-05-18 18:48:11 +0530

    this is new commit

---
+++ src/main/java/Swapneshpatelacedemy/NewCode.java
@@ -0,0 +1,15 @@
+package Swapneshpatelacedemy;
+
+
+public class NewCode {
+	
+	public void postjira() {
+		System.out.println("first post");
+		System.out.println("second post");
+		System.out.println("third post");
+
+
+		
+	}
+
+}
