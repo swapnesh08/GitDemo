7a2c209510a00b7adb025d555e1585da6927a122
swapnesh <swapneshhessonite@gmail.com>
2025-05-19 10:35:21 +0530

    this is again new commit from GitDemo

--- src/main/java/Swapneshpatelacedemy/NewCode.java
+++ src/main/java/Swapneshpatelacedemy/NewCode.java
@@ -7,6 +7,10 @@ public class NewCode {
 		System.out.println("first post");
 		System.out.println("second post");
 		System.out.println("third post");
+		
+		System.out.println("fourth post");
+
+		
 
 
 		
